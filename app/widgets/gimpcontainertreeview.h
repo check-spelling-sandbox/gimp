bcaeeb008eb49222cfe4ddb82e8215f54bd22bac
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-06-25 22:32:23 -0400

    spelling: empty
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- app/widgets/gimpcontainertreeview.h
+++ app/widgets/gimpcontainertreeview.h
@@ -127,7 +127,7 @@ void        gimp_container_tree_view_add_renderer_cell
 
 void        gimp_container_tree_view_set_dnd_drop_to_empty
                                               (GimpContainerTreeView *tree_view,
-                                               gboolean               dnd_drop_to_emtpy);
+                                               gboolean               dnd_drop_to_empty);
 void        gimp_container_tree_view_connect_name_edited
                                               (GimpContainerTreeView *tree_view,
                                                GCallback              callback,
